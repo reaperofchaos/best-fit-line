39eae2920d79ae5466af561290fa67abf1785712
reaperofchaos <tokugawa111@gmail.com>
2023-11-18 11:31:20 -0500

    create functions to solve matrix

--- semester-project/src/utils/matrix_operations.rs
+++ semester-project/src/utils/matrix_operations.rs
@@ -1,5 +1,14 @@
 #[path = "tests/matrix_operations_test.rs"] mod matrix_operations_tests;
 
+/// builds a matrix with placeholder values
+///  for the rows of left and columns of right
+/// Description.
+/// 
+/// * `lhs` - first vector being multiplied
+/// * `rhs` - second vector being multiplied
+/// 
+/// Return.
+/// * matrix Vec<Vec<f64>>
 fn initialize_result_matrix(lhs: Vec<Vec<f64>>, rhs: Vec<Vec<f64>>)-> Vec<Vec<f64>>{
 let mut result_vec: Vec<Vec<f64>> = vec![];
     let mut a = 0;
@@ -30,7 +39,15 @@ let mut result_vec: Vec<Vec<f64>> = vec![];
 
     return result_vec; 
 }
+
 /// Performs matrix multiplication
+/// Description.
+/// 
+/// * `lhs` - first vector being multiplied
+/// * `rhs` - second vector being multiplied
+/// 
+/// Return.
+/// * matrix Vec<Vec<f64>>
 fn matrix_multiplication(lhs: Vec<Vec<f64>>, rhs: Vec<Vec<f64>>)-> Vec<Vec<f64>>{
     // columns of left equal to rows on right
     let mut result_vec = initialize_result_matrix(lhs.clone(), rhs.clone()); 
@@ -51,4 +68,199 @@ fn matrix_multiplication(lhs: Vec<Vec<f64>>, rhs: Vec<Vec<f64>>)-> Vec<Vec<f64>>
     }
 
     return result_vec; 
+}
+
+/// Finds row with largest value
+/// Description.
+/// finds the index of the row
+/// with the largest value at 
+// the column index
+/// 
+/// * `matrix` -  vector being multiplied
+/// * `col_index` - column index finding the largest value
+/// 
+/// Return.
+/// * matrix Vec<Vec<f64>>
+fn find_largest_row_by_col(matrix: Vec<Vec<f64>>, col_index: usize)->usize{
+    println!{"col index {}", col_index};
+
+    let mut largest = matrix[col_index][col_index];
+    println!{"value at col index {} row index {} {}",col_index, col_index, largest};
+
+    let mut largestIndex = col_index;
+    let mut i = col_index; 
+
+    if matrix.len() > 0 && matrix[0].len() >= col_index {
+        while i < matrix.len() {
+            println!("row {} value at col index {} {} ", i, col_index, matrix[i][col_index]);
+
+            if matrix[i][col_index] > largest{
+                println!("largest value is now {} and at row {}", matrix[i][col_index], i);
+                largestIndex = i;
+                largest = matrix[i][col_index];
+            }
+            i += 1; 
+        }
+    }
+    
+    println!("largest value is {}", largestIndex);
+    return largestIndex;
+}
+
+/// Swap rows at indexes
+/// Description.
+/// 
+/// * `matrix` -  vector being multiplied
+/// * `from` - row index swapping from
+/// * `to` - row index swapping to
+/// 
+/// Return.
+/// * matrix Vec<Vec<f64>>
+fn swap_row(mut matrix: Vec<Vec<f64>>, from: usize, to: usize)->Vec<Vec<f64>>{
+    if matrix.len() > from && matrix.len() > to{
+        matrix.swap(from, to); 
+
+        return matrix; 
+    }
+
+    return matrix;
+}
+
+/// Scale a row in a matrix
+/// Description.
+/// 
+/// * `matrix` -  matrix scaling a row on
+/// * `row_index` - row index of row to scale
+/// * `scale` - value to scale on
+/// 
+/// Return.
+/// * matrix Vec<Vec<f64>>
+fn scale_row(mut matrix: Vec<Vec<f64>>, row_index: usize, scale: f64)->Vec<Vec<f64>>{
+    let mut i = 0; 
+    while i < matrix[row_index].len(){
+        if matrix[row_index][i] == 0.0{
+
+            i += 1;
+            continue;
+        }
+
+        matrix[row_index][i] = scale * matrix[row_index][i];
+        i += 1; 
+    }
+
+    return matrix; 
+}
+
+
+/// Eliminate a row by subtracting each row by a scaled
+/// source row
+/// Description.
+/// 
+/// * `matrix` -  matrix scaling a row on
+/// * `row_index` - row index of row to eliminate
+/// 
+/// Return.
+/// * matrix Vec<Vec<f64>>
+fn eliminate_row(mut matrix: Vec<Vec<f64>>, row_index: usize)->Vec<Vec<f64>>{
+    if row_index < matrix.len() {
+        let mut i = row_index + 1; 
+        while i < matrix.len(){
+            let s = matrix[i][row_index];
+
+            let mut j = row_index; 
+            while j < matrix[i].len(){
+                matrix[i][j] = matrix[i][j] - s * matrix[row_index][j];
+                j += 1; 
+            }
+            
+            matrix[i][row_index] = 0.0;
+            i += 1; 
+        }
+    }
+
+    return matrix; 
+} 
+
+/// Function to backsolve a matrix in row
+/// echelon form
+/// Description.
+/// 
+/// * `matrix` -  matrix scaling a row on
+/// 
+/// Return.
+/// * matrix Vec<Vec<f64>>
+fn back_solve_matrix(mut matrix: Vec<Vec<f64>>)->Vec<Vec<f64>>{
+    println!("total rows {}", matrix.len());
+    if matrix.len() > 0 {
+        let last_row_index = matrix.len() -1;
+        let last_col = matrix[0].len() - 1;
+
+        let mut i = last_row_index;
+        while i > 0 {
+            let mut j = i -1; 
+            loop {
+                let s = matrix[j][i];
+
+                matrix[j][i] -= s * matrix[i][i];
+                matrix[j][last_col] -= s * matrix[i][last_col];
+
+                if j == 0 {
+                    break;
+                }
+                
+                j -= 1; 
+            }
+            
+            i -= 1; 
+        }
+    }
+    return matrix;
+}
+
+/// function to convert a matrix to
+/// row echelon form
+/// Description.
+/// 
+/// * `matrix` -  matrix scaling a row on
+/// 
+/// Return.
+/// * matrix Vec<Vec<f64>>
+fn convert_matrix_to_row_echelon_form(matrix: Vec<Vec<f64>>)->Vec<Vec<f64>>{
+    let mut updatedMatrix = matrix.clone(); 
+    // Iterate through all rows
+    let mut i = 0;
+    while i < updatedMatrix.len(){
+        //  Pivot
+        let largestRow = find_largest_row_by_col(updatedMatrix.clone(), i);
+        updatedMatrix = swap_row(updatedMatrix.clone(), largestRow, i);
+
+        // scale if column of interest at position is not 1
+        if updatedMatrix[i][i] != 1.0{
+            let scale = 1.0/updatedMatrix[i][i];
+            updatedMatrix = scale_row(updatedMatrix.clone(), i, scale);
+        }
+
+        updatedMatrix[i][i] = 1.0; 
+
+        if i == 0 || (i > 0  && updatedMatrix[i][i] != 0.0){
+            updatedMatrix = eliminate_row(updatedMatrix.clone(), i); 
+        }
+
+        i += 1;
+    }
+    return updatedMatrix; 
+}
+
+/// function to convert a matrix to
+/// reduced row echelon form
+/// Description.
+/// 
+/// * `matrix` -  matrix scaling a row on
+/// 
+/// Return.
+/// * matrix Vec<Vec<f64>>
+fn convert_matrix_to_reduced_row_echelon_form(matrix: Vec<Vec<f64>>)->Vec<Vec<f64>>{
+    let updatedMatrix = convert_matrix_to_row_echelon_form(matrix);
+
+    return back_solve_matrix(updatedMatrix);
 }
--- semester-project/src/utils/tests/matrix_operations_test.rs
+++ semester-project/src/utils/tests/matrix_operations_test.rs
@@ -16,6 +16,7 @@ mod matrix_operation_tests {
 
         assert_eq!(matrix_operations::matrix_multiplication(test_1, test_2), expected);
     }
+    
     #[test]
     fn test_matrix_multiplication_2_x_2() {
         let test_row_1: Vec<f64> = vec![1.0, 1.0];
@@ -28,6 +29,92 @@ mod matrix_operation_tests {
         let expected: Vec<Vec<f64>> = vec![expected_row.clone(), expected_row.clone()];
 
         assert_eq!(matrix_operations::matrix_multiplication(lhs, rhs), expected);
+    }
+
+    #[test]
+    fn test_find_largest_row_by_col() {
+        let test_row_1: Vec<f64> = vec![0.0, 1.0, 3.0];
+        let test_row_2: Vec<f64> = vec![1.0, 2.0, 2.0];
+        let test_row_3: Vec<f64> = vec![1.0, 3.0, 2.0];
+        let test: Vec<Vec<f64>> = vec![test_row_1, test_row_2, test_row_3];
+
+        assert_eq!(matrix_operations::find_largest_row_by_col(test.clone(), 0), 1);
+        assert_eq!(matrix_operations::find_largest_row_by_col(test.clone(), 1), 2);
+        assert_eq!(matrix_operations::find_largest_row_by_col(test.clone(), 2), 2);
+    }
+
+    #[test]
+    fn test_row_swap() {
+        let test_row_1: Vec<f64> = vec![0.0, 1.0, 3.0];
+        let test_row_2: Vec<f64> = vec![1.0, 2.0, 2.0];
+        let test_row_3: Vec<f64> = vec![1.0, 3.0, 2.0];
+        let test: Vec<Vec<f64>> = vec![test_row_1.clone(), test_row_2.clone(), test_row_3.clone()];
+        let expected: Vec<Vec<f64>> = vec![test_row_3, test_row_2, test_row_1];
+
+        assert_eq!(matrix_operations::swap_row(test, 0, 2), expected);
+    }
+
+        #[test]
+    fn test_scale_row() {
+        let test_row_1: Vec<f64> = vec![0.0, 1.0, 3.0];
+        let test_row_2: Vec<f64> = vec![1.0, 2.0, 2.0];
+        let test_row_3: Vec<f64> = vec![1.0, 3.0, 2.0];
+        let scaled_test_row_3: Vec<f64> = vec![3.0, 9.0, 6.0];
+
+        let test: Vec<Vec<f64>> = vec![test_row_1.clone(), test_row_2.clone(), test_row_3.clone()];
+        let expected: Vec<Vec<f64>> = vec![test_row_1, test_row_2, scaled_test_row_3];
+
+        assert_eq!(matrix_operations::scale_row(test, 2, 3.0), expected);
+    }
+
+    #[test]
+    fn test_eliminate_row() {
+        let test_row_1: Vec<f64> = vec![1.0, 7.0, 55.0];
+        let test_row_2: Vec<f64> = vec![11.0, 55.0, 385.0];
+        let eliminated_row_2: Vec<f64> = vec![0.0, -22.0, -220.0];
+
+        let test: Vec<Vec<f64>> = vec![test_row_1.clone(), test_row_2.clone()];
+        let expected: Vec<Vec<f64>> = vec![test_row_1, eliminated_row_2];
+
+        assert_eq!(matrix_operations::eliminate_row(test, 0), expected);
+    }
+
+    #[test]
+    fn test_back_solve_matrix() {
+        let test_row_1: Vec<f64> = vec![1.0, 7.0, 55.0];
+        let test_row_2: Vec<f64> = vec![0.0, 1.0, 10.0];
+        let back_solve_row_1: Vec<f64> = vec![1.0, 0.0, -15.0];
+
+        let test: Vec<Vec<f64>> = vec![test_row_1.clone(), test_row_2.clone()];
+        let expected: Vec<Vec<f64>> = vec![back_solve_row_1, test_row_2];
+
+        assert_eq!(matrix_operations::back_solve_matrix(test), expected);
+    }
+
+    #[test]
+    fn test_convert_matrix_to_row_echelon_form() {    
+        let test_row_1: Vec<f64> = vec![11.0, 55.0, 385.0];
+        let test_row_2: Vec<f64> = vec![55.0, 385.0, 3025.0];
+        let solve_row_1: Vec<f64> = vec![1.0, 7.0, 55.0];
+        let solve_row_2: Vec<f64> = vec![0.0, 1.0, 10.0];
+
+        let test: Vec<Vec<f64>> = vec![test_row_1.clone(), test_row_2.clone()];
+        let expected: Vec<Vec<f64>> = vec![solve_row_1, solve_row_2];
+
+        assert_eq!(matrix_operations::convert_matrix_to_row_echelon_form(test), expected);
+    }
+
+    #[test]
+    fn test_convert_matrix_to_reduced_row_echelon_form() {    
+        let test_row_1: Vec<f64> = vec![11.0, 55.0, 385.0];
+        let test_row_2: Vec<f64> = vec![55.0, 385.0, 3025.0];
+        let solve_row_1: Vec<f64> = vec![1.0, 0.0, -15.0];
+        let solve_row_2: Vec<f64> = vec![0.0, 1.0, 10.0];
+
+        let test: Vec<Vec<f64>> = vec![test_row_1.clone(), test_row_2.clone()];
+        let expected: Vec<Vec<f64>> = vec![solve_row_1, solve_row_2];
+
+        assert_eq!(matrix_operations::convert_matrix_to_reduced_row_echelon_form(test), expected);
     }
 
 }
